1fcdd717fc102a435db26066e70abe92c73804e3
erickjosue1 <josuezerod@gmail.com>
2023-05-29 12:26:08 -0600

    Fix4

--- resources/js/Pages/Proposals/Index.vue
+++ resources/js/Pages/Proposals/Index.vue
@@ -227,13 +227,13 @@ export default {
                                 </span>
                             </BaseButtons>
                             <BaseButtons v-else-if="useRole('Admin')" type="justify-center lg:justify-end" no-wrap>
-                                <a  v-if="item.evaluador_id" :href="route(`${routeName}assignment`, item.id)"> <button
+                                <a  v-if="item.evaluador_id == null" :href="route(`${routeName}assignment`, item.id)"> <button
                                         class="bg-transparent hover:bgeve-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
                                         Asignar Revisor
                                     </button>
                                 </a>
                                 <div v-else>
-                                    {{ item.evaluador_id }}
+                                    Ya asignado
                                 </div>
                             </BaseButtons>
                         </td>
